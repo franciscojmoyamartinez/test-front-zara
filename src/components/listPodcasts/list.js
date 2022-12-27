6a8b54273502eac3c8e79cb5a9cfa179070b5dc7
Francisco Javier <franciscojmoyamartinez@gmail.com>
2022-12-27 18:33:21 +0100

    feat(listado): Inicialización de listado

---
+++ src/components/listPodcasts/list.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import listReq from '../../api/getListPodcast';
+import template from '../../views/listPodcasts/list.jsx';
+
+class ListPodcast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewList: false,
+      data: [],
+    };
+  }
+  componentDidMount(){
+    this.listRequest(this);
+  }
+
+  listRequest() {
+    listReq()
+      .then((res) => {
+        // set the response in class states
+        const dataArray = [];
+        if (res.data.feed.entry.length > 0) {
+          res.data.feed.entry.forEach((element) => {
+            dataArray.push({
+              'name' : element.summary
+            });
+          });
+          this.setState({
+            data: dataArray,
+            viewList: true,
+          });
+        } else {
+          this.setState({
+            viewList: true,
+            data: [],
+          });
+        }
+      })
+      .catch(() => {
+      });
+  }
+
+  render() {
+    return template(this);
+  }
+}
+
+
+export default ListPodcast
