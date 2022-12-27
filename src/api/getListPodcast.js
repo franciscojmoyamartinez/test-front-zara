d6fa7d373e16b09c8e08a3510c0712fdf9813a90
Francisco Javier <franciscojmoyamartinez@gmail.com>
2022-12-27 18:33:40 +0100

    feat(listado): Llamada a endpoint api itunes

---
+++ src/api/getListPodcast.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+import gConf from '../conf/conf';
+
+const listPodCast = () => {
+    const payload = {
+    };
+
+    return axios.get(`${gConf.urlApi}`, payload);
+};
+
+export default listPodCast;
