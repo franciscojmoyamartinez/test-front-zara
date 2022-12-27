56bdfea957bfc5690a8013d82119e544a3594d22
Francisco Javier <franciscojmoyamartinez@gmail.com>
2022-12-27 18:32:24 +0100

    feat(conf): Fichero rutas

---
+++ src/conf/conf.js
@@ -0,0 +1,23 @@
+const globalConf = {
+    env: 'development',
+    urlApi: 'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json',
+    urlApiObtainDetailPodCast: ' https://itunes.apple.com/lookup?id=',
+  };
+  
+// return env conf; develop or production
+const envFunction = () => {
+    // OVERRIDES
+    if (process.env.NODE_ENV) {
+      globalConf.env = process.env.NODE_ENV;
+    }
+    if (process.env.REACT_APP_URLAPI) {
+      globalConf.urlApi = process.env.REACT_APP_URLAPI;
+    }
+    if (process.env.REACT_APP_URLAPIOBTAINDETAIL) {
+      globalConf.urlApiObtainDetailPodCast = process.env.REACT_APP_URLAPIOBTAINDETAIL;
+    }
+    return globalConf;
+};
+
+export default envFunction();
+  
