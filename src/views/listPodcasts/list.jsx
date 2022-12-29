6d77485f7348ac091e552514f05eca052f89d6c6
Francisco Javier <franciscojmoyamartinez@gmail.com>
2022-12-29 12:52:46 +0100

    feat(podcast): aplicar search bar

--- src/views/listPodcasts/list.jsx
+++ src/views/listPodcasts/list.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import SearchBar from '../../components/listPodcasts/SearchBar';
 import Item
- from './item';
+  from './item';
 const template = (component) => {
+
   const {
     data,
   } = component.state;
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        {data.map((podcast, index) => (
-          <Grid item md={3} key={index} className='grid-box'>
-            <Item podcast={podcast}></Item>
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item md={12} className='grid-box'>
+            <span>{data.length}</span>
+            <SearchBar/>
           </Grid>
-        ))}
-      </Grid>
-    </Box>
+          {data.map((podcast, index) => (
+            <Grid item md={3} key={index} className='grid-box'>
+              <Item podcast={podcast}></Item>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </div>
   );
 };
 
