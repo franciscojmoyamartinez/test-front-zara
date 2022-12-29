8045df8d1843ef73c9c305d757016d997beea98d
Francisco Javier <franciscojmoyamartinez@gmail.com>
2022-12-29 20:58:20 +0100

    feat(searchbar): buscador

--- src/components/listPodcasts/SearchBar.js
+++ src/components/listPodcasts/SearchBar.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
 const SearchBar = ({keyword, onChange}) => {
+    const [inputText, setInputText] = useState("");
     const BarStyle = {width:"20rem",background:"#F0F0F0", border:"none", padding:"0.5rem"};
+    let inputHandler = (e) => {
+      //convert input text to lower case
+      let lowerCase = e.target.value.toLowerCase();
+      setInputText(lowerCase);
+    };
     return (
       <input 
        style={BarStyle}
        key="search-bar"
-       value={keyword}
+       value={inputText}
        placeholder={"Filter Podcast..."}
-       onChange={(e) => onChange(e.target.value)}
+       onChange={inputHandler}
       />
     );
   }
